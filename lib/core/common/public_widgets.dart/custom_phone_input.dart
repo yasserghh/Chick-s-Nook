2545b64a97c8465dce553a322fd3fd0c37d41a35
Tahar Chaouch Yasser <taharchaouchyasser867@gmail.com>
2024-11-23 19:40:11 +0100

    fift commit

--- lib/core/common/public_widgets.dart/custom_phone_input.dart
+++ lib/core/common/public_widgets.dart/custom_phone_input.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import '../../resources/color_manager.dart';
 import '../../resources/fonts_manager.dart';
 import '../../resources/styles_manager.dart';
 
-Widget getCustomPhoneInput(
-    {required String hint,
-    required String? Function(String?)? validator,
-    required void Function(PhoneNumber)? onInputChanged}) {
+Widget getCustomPhoneInput({
+  required String hint,
+  required String? Function(String?)? validator, required Null Function(dynamic e) onInputChanged,
+ // required void Function(PhoneNumber)? onInputChanged
+}) {
+
+ // InternationalPhoneNumberInput(
+         //formatInput: false,
+         // countries: const ['DZ'],
+         // validator: validator,
+         // hintText: hint,
+        //  keyboardType: TextInputType.phone,
+        //  textAlign: TextAlign.right,
+        //  textStyle:
+        //      getSemiBoldStyle(16, ColorManager.primary, FontsConstants.cairo),
+         // inputDecoration: InputDecoration(
+           //   constraints: const BoxConstraints(maxHeight: 60, minHeight: 60),
+           //   contentPadding: const EdgeInsets.only(bottom: 5),
+           //   errorStyle:
+            //      getRegularStyle(10, ColorManager.reed, FontsConstants.cairo),
+          //    hintText: hint,
+          //    border: InputBorder.none,
+            //  hintStyle: getSemiBoldStyle(
+         //         14, ColorManager.greyBlue, FontsConstants.cairo)),
+         // onInputChanged: onInputChanged,
+      //  )
   return Container(
       height: 56,
       width: double.infinity,
@@ -15,25 +36,6 @@ Widget getCustomPhoneInput(
           color: ColorManager.grey2, borderRadius: BorderRadius.circular(40)),
       child: Padding(
         padding: const EdgeInsets.only(left: 20, right: 20),
-        child: InternationalPhoneNumberInput(
-          formatInput: false,
-          countries: const ['DZ'],
-          validator: validator,
-          hintText: hint,
-          keyboardType: TextInputType.phone,
-          textAlign: TextAlign.right,
-          textStyle:
-              getSemiBoldStyle(16, ColorManager.primary, FontsConstants.cairo),
-          inputDecoration: InputDecoration(
-              constraints: const BoxConstraints(maxHeight: 60, minHeight: 60),
-              contentPadding: const EdgeInsets.only(bottom: 5),
-              errorStyle:
-                  getRegularStyle(10, ColorManager.reed, FontsConstants.cairo),
-              hintText: hint,
-              border: InputBorder.none,
-              hintStyle: getSemiBoldStyle(
-                  14, ColorManager.greyBlue, FontsConstants.cairo)),
-          onInputChanged: onInputChanged,
-        ),
+        child: null,
       ));
 }
